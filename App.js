6228e02a73c8067a86c717126b1d87e05b4a0599
stepan.litvinov <stepan.litvinov@nexign.com>
2020-05-17 19:45:06 +0300

    authorization

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React from 'react';
 import {GiftedChat} from 'react-native-gifted-chat';
+import {authorize} from 'react-native-app-auth';
 
 class App extends React.Component {
   state = {
@@ -15,20 +16,54 @@ class App extends React.Component {
   };
 
   componentDidMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Hello developer',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-      ],
-    });
+    const config = {
+      issuer: 'http://192.168.1.44:8080/auth/realms/chat',
+      clientId: 'chat',
+      redirectUrl: 'com.sweetchat:/callback',
+      scopes: ['openid', 'profile'],
+      dangerouslyAllowInsecureHttpRequests: true,
+    };
+
+    // // Refresh token
+    // const refreshedState = await refresh(config, {
+    //   refreshToken: authState.refreshToken,
+    // });
+
+    authorize(config)
+      .then(result => {
+        // result includes accessToken, accessTokenExpirationDate and refreshToken
+
+        this.setState({
+          messages: [
+            {
+              _id: 1,
+              text: JSON.stringify(result),
+              createdAt: new Date(),
+              user: {
+                _id: 2,
+                name: 'Success',
+                avatar: 'https://placeimg.com/140/140/any',
+              },
+            },
+          ],
+        });
+      })
+      .catch(error => {
+        this.setState({
+          messages: [
+            {
+              _id: 1,
+              text: JSON.stringify(error),
+              createdAt: new Date(),
+              user: {
+                _id: 2,
+                name: 'Error',
+                avatar: 'https://placeimg.com/140/140/any',
+              },
+            },
+          ],
+        });
+      });
   }
 
   onSend(messages = []) {
@@ -38,7 +73,7 @@ class App extends React.Component {
   }
 
   render() {
-    return (
+    return this.state.messages.length ? (
       <GiftedChat
         messages={this.state.messages}
         onSend={messages => this.onSend(messages)}
@@ -46,7 +81,7 @@ class App extends React.Component {
           _id: 1,
         }}
       />
-    );
+    ) : null;
   }
 }
 
---
+++ scripts/generateTls.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out tls.crt -keyout tls.key
